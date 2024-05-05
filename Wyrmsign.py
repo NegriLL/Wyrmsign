db8de1aa45c22033a2c5eb11cee477cafad5481b
NegriLL <leonardo.negri@helsinki.fi>
2024-05-05 20:54:55 -0300

    improve formating

--- Wyrmsign.py
+++ Wyrmsign.py
@@ -1,8 +1,8 @@
+import math
 from PIL import Image, ImageFilter
 from configparser import ConfigParser
-import math
 
-def readConfig():
+def read_config():
     print("Reading configuration file...")
     file = "settings.ini"
     config = ConfigParser()
@@ -39,11 +39,9 @@ def readConfig():
     global CROP_ROOM
     CROP_ROOM = math.floor(IMAGE_SIZE - 2*HEXAGON_APOTHEM)
 
-
-
-def readAlphabet(vowels, consonants, modifiers, breaks):
-    # Reads the file in ALPHABET_FILE and returns an array of with
-    # the symbols for each individual letter.
+def read_alphabet(vowels, consonants, modifiers, breaks):
+    """ Reads the file in ALPHABET_FILE and returns an array of with
+    the symbols for each individual letter."""
 
     file = open(ALPHABET_FILE, "r", encoding="utf-8")
     n = 0
@@ -60,22 +58,22 @@ def readAlphabet(vowels, consonants, modifiers, breaks):
         # Begin drawing each letter and assigning it to its corresponding
         # array
         if(type == "v"):
-            vowels[letter] = drawSymbol(parts)
+            vowels[letter] = draw_symbol(parts)
         elif(type == "c"):
-            consonants[letter] = drawSymbol(parts)
+            consonants[letter] = draw_symbol(parts)
         elif(type == "m"):
-            modifiers[letter] = drawSymbol(parts)
+            modifiers[letter] = draw_symbol(parts)
         elif(type == "b"):
-            breaks[letter] = drawSymbol(parts)
-
+            breaks[letter] = draw_symbol(parts)
         n += 1
     print("Reading Dictionary. Letters Read: {}".format(n))
-
+    file.close()
     return [vowels, consonants, modifiers, breaks]
 
-def readText(vowels, consonants, modifiers, breaks):
-    # Main text parser function. Reads the text in TEXT_NAME and returns
-    # the list of symbols that need to be printed
+def read_text(vowels, consonants, modifiers, breaks):
+    """ Main text parser function. Reads the text in TEXT_NAME and returns
+    the list of symbols that need to be printed"""
+
     print("Reading Text...")
     textFile = open(TEXT_NAME, "r", encoding="utf-8")
     symbols = []
@@ -84,24 +82,20 @@ def readText(vowels, consonants, modifiers, breaks):
 
     for line in textFile:
         line = line.strip("\n")
-
         for x in line:
             # Replaces all unknown symbols with a symbol that will later be substituted
             # by a blank.
-            if(not checkLetter(x, vowels, consonants, modifiers, breaks)):
+            if not check_letter(x, vowels, consonants, modifiers, breaks):
                 k+=1
                 print("Unknown symbols removed: {}".format(k), end="\r")
                 line = line.replace(x,"$")
-        
         print()
+
         while len(line) > 0:  # Main parser loop
-
             print("Symbols created: {}".format(n), end="\r")
-            
             parts = []
-
             # Check if symbol is unknown and draws UnknownSymbol
-            if(line[0] == "$"):
+            if line[0] == "$":
                 symbol = Image.new('RGBA',(IMAGE_SIZE, IMAGE_SIZE),(255, 255, 255, 0))
                 symbol.paste(modifiers["UnknownSymbol"],(0,0),modifiers["UnknownSymbol"])
                 symbols.append(symbol)
@@ -112,49 +106,49 @@ def readText(vowels, consonants, modifiers, breaks):
             
             c = False
             # Checks if a consonant is present first
-            if(len(line) >= 2): # Double consonants are a special case that must be checked here
-                if(line[0].lower() in consonants and (line[0].lower() == line[1].lower())):
+            if len(line) >= 2: # Double consonants are a special case that must be checked here
+                if line[0].lower() in consonants and (line[0].lower() == line[1].lower()):
                     c = True
                     parts.append(consonants[line[0]])
                     parts.append(modifiers["ModifierLongConsonant"])
                     line = line[2:]
-                elif(line[0].lower() in consonants):
+                elif line[0].lower() in consonants:
                     c = True
                     parts.append(consonants[line[0].lower()])
                     line = line[1:]
-            elif(len(line) >= 1): # Checking for single consonants
-                if(line[0].lower() in consonants):
+            elif len(line) >= 1: # Checking for single consonants
+                if line[0].lower() in consonants:
                     c = True
                     parts.append(consonants[line[0].lower()])
                     line = line[1:]
 
             # Check if a vowel is present afterwards.
             v = False
-            if(len(line) >= 2): # Double vowels are a special case that must be checked here
-                if(line[0].lower() + line[1].lower() in vowels):
+            if len(line) >= 2: # Double vowels are a special case that must be checked here
+                if line[0].lower() + line[1].lower() in vowels:
                     v = True
                     parts.append(vowels[line[0].lower() + line[1].lower()])
                     line = line[2:]
-                elif(line[0].lower() in vowels):
+                elif line[0].lower() in vowels:
                     v = True
                     parts.append(vowels[line[0].lower()])
                     line = line[1:]
-            elif(len(line) >= 1): # Checking for single consonants
-                if(line[0].lower() in vowels):
+            elif len(line) >= 1: # Checking for single consonants
+                if line[0].lower() in vowels:
                     v = True
                     parts.append(vowels[line[0].lower()])
                     line = line[1:]
             
             # Check if there is a break such as a space or a period
-            if(len(line) >= 1):
-                if(line[0] in breaks):
+            if len(line) >= 1:
+                if line[0] in breaks:
                     parts.append(breaks[line[0]])
                     line = line[1:]
 
             # Puts everything together, accounting for the lack of consonants or vowels
-            if(not c):
+            if not c:
                 parts.append(modifiers["ModifierNoConsonant"])
-            elif(not v):
+            elif not v:
                 parts.append(modifiers["ModifierNoVowel"])
 
             # The symbols are all put  together into the final syllable.
@@ -165,9 +159,10 @@ def readText(vowels, consonants, modifiers, breaks):
             n += 1
 
     print("Symbols created: {}".format(n))
+    textFile.close()
     return symbols
 
-def checkLetter(letter, vowels, consonants, modifiers, breaks):
+def check_letter(letter, vowels, consonants, modifiers, breaks):
     # Function that checks if a letter is found in any of the dictionaries
     letter = letter.lower()
     if letter in vowels:
@@ -181,7 +176,7 @@ def checkLetter(letter, vowels, consonants, modifiers, breaks):
     else:
         return False
 
-def drawSymbol(numbers):
+def draw_symbol(numbers):
     # Takes numbers in an array and joins them into an image by using the corresponding
     # Parts/(number).png files.
     symbol = Image.new('RGBA', (IMAGE_SIZE,IMAGE_SIZE),(255, 255, 255, 0))
@@ -196,7 +191,7 @@ def drawSymbol(numbers):
     return symbol
 
 
-def drawText(symbols):
+def draw_text(symbols):
     # Takes the list of symbols and transforms it into the contiguous text.
     collumns = MAX_CHARACTERS_PER_LINE
     if(len(symbols) < MAX_CHARACTERS_PER_LINE):
@@ -205,9 +200,9 @@ def drawText(symbols):
     rows = math.ceil(len(symbols) / collumns)
     n = 0
 
-    image = createCanvas(rows, collumns)
+    image = create_canvas(rows, collumns)
     if(GUIDES):
-        image = drawGuide(rows, collumns, len(symbols), image)
+        image = draw_guide(rows, collumns, len(symbols), image)
 
     for r in range(0,rows):
         for c in range(0,collumns):
@@ -216,7 +211,8 @@ def drawText(symbols):
                 print("Working on image {}/{}".format(n,len(symbols)))
                 return image
             # Decide the placement of each symbol with a bit of math. Hexagons are fun.
-            x = math.ceil(2*c*HEXAGON_APOTHEM - c*LINE_WIDTH - (r%2)*LINE_WIDTH/2 + HEXAGON_APOTHEM*(r%2))
+            x = math.ceil(2*c*HEXAGON_APOTHEM - c*LINE_WIDTH
+                          - (r%2)*LINE_WIDTH/2 + HEXAGON_APOTHEM*(r%2))
             y = math.ceil(r*(HEXAGON_SIDE - LINE_WIDTH/2)*3/2)
             image.paste(symbols[n], (x, y), symbols[n])
             n += 1
@@ -224,7 +220,7 @@ def drawText(symbols):
     print("Working on image {}/{}".format(n,len(symbols)))
     return image
 
-def createCanvas(rows, columns):
+def create_canvas(rows, columns):
     # Creates the full size of the final image with a bit of hexagon math. Since the hexagon
     # edges must overlap each other and each even numbered line has to be displaced, it
     # requires some math.
@@ -232,13 +228,14 @@ def createCanvas(rows, columns):
     w = math.ceil(2*HEXAGON_APOTHEM*columns - LINE_WIDTH*(columns-1) - LINE_WIDTH/2)
     if(rows > 1):
         w += math.ceil(HEXAGON_APOTHEM)
-    h = math.ceil(rows*(HEXAGON_SIDE - LINE_WIDTH/2)*3/2 + HEXAGON_SMALL_HEIGHT + LINE_WIDTH)
+    h = math.ceil(rows*(HEXAGON_SIDE - LINE_WIDTH/2)*3/2
+                  + HEXAGON_SMALL_HEIGHT + LINE_WIDTH)
 
     canvas = Image.new('RGBA',(w, h),(255, 255, 255, 255))
 
     return canvas
 
-def drawGuide(rows, collumns, length, canvas):
+def draw_guide(rows, collumns, length, canvas):
     # If GUIDES = True, then this function will draw guidelines for the placement of each
     # hexagonal symbol.
     guide = Image.open('Parts\guide.png')
@@ -252,7 +249,8 @@ def drawGuide(rows, collumns, length, canvas):
                 print("Working on guides {}/{}".format(n,length))
                 return canvas
             # TODO: Implement the following code as a function to avoid rewriting it.
-            x = math.ceil(2*c*HEXAGON_APOTHEM - c*LINE_WIDTH - (r%2)*LINE_WIDTH/2 + HEXAGON_APOTHEM*(r%2))
+            x = math.ceil(2*c*HEXAGON_APOTHEM - c*LINE_WIDTH - (r%2)*LINE_WIDTH/2
+                          + HEXAGON_APOTHEM*(r%2))
             y = math.ceil(r*(HEXAGON_SIDE - LINE_WIDTH/2)*3/2)
             canvas.paste(guide, (x, y), guide)
             n += 1
@@ -262,17 +260,17 @@ def drawGuide(rows, collumns, length, canvas):
 
 
 def main():
-    readConfig()
+    read_config()
 
     vowels = {}
     consonants = {}
     modifiers = {}
     breaks = {}
+    [vowels, consonants, modifiers, breaks] = read_alphabet(vowels, consonants,
+                                                            modifiers, breaks)
 
-    [vowels, consonants, modifiers, breaks] = readAlphabet(vowels, consonants, modifiers, breaks)
-
-    symbols = readText(vowels, consonants, modifiers, breaks)
-    result = drawText(symbols)
+    symbols = read_text(vowels, consonants, modifiers, breaks)
+    result = draw_text(symbols)
 
     if(RESIZE): 
         print("Resizing...")
