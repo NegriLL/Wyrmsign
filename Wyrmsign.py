9f66f947c65c3dcf3249acd9294e877d4ddf423f
NegriLL <leonardo.negri@helsinki.fi>
2024-05-05 22:02:11 -0300

    remove global variables

--- Wyrmsign.py
+++ Wyrmsign.py
@@ -8,42 +8,27 @@ def read_config():
     config = ConfigParser()
     config.read(file)
 
-    # User options
-    global TEXT_NAME
-    TEXT_NAME = config["UserSettings"]["TextFile"]
-    global GUIDES
-    GUIDES = config["UserSettings"].getboolean("Guides")
-    global RESIZE
-    RESIZE = config["UserSettings"].getboolean("Resize")
-    global RESIZE_PERCENT
-    RESIZE_PERCENT = config["UserSettings"].getfloat("ResizePercent")
-    global MAX_CHARACTERS_PER_LINE
-    MAX_CHARACTERS_PER_LINE = config["UserSettings"].getint("CharactersPerLine")
+    # For image formating
+    hexagonSide = int(config["ProgramSettings"].getint("ImageSize")/2)
+    hexagonApothem = int(math.sqrt(hexagonSide**2 - (hexagonSide**2)/4))
+    hexagonSmallHeight = int(math.sqrt(hexagonSide**2 - hexagonApothem**2))
+    cropRoom = int(math.floor(config["ProgramSettings"].getint("ImageSize")
+                              - 2*hexagonApothem))
 
+    config.add_section("HexagonMath")
+    config.set("HexagonMath", "HexagonSide", str(hexagonSide))
+    config.set("HexagonMath", "HexagonApothem", str(hexagonApothem))
+    config.set("HexagonMath", "HexagonSmallHeight", str(hexagonSmallHeight))
+    config.set("HexagonMath", "CropRoom", str(cropRoom))
+                           
+    return config
 
-    # Program options
-    global ALPHABET_FILE
-    ALPHABET_FILE = config["ProgramSettings"]["AlphabetFile"]
-    global IMAGE_SIZE
-    IMAGE_SIZE = config["ProgramSettings"].getint("ImageSize")
-    global LINE_WIDTH
-    LINE_WIDTH = config["ProgramSettings"].getint("LineWidth")
-
-    # Perform calculations for image formatting
-    global HEXAGON_SIDE
-    HEXAGON_SIDE = IMAGE_SIZE/2
-    global HEXAGON_APOTHEM
-    HEXAGON_APOTHEM = math.sqrt(HEXAGON_SIDE**2 - (HEXAGON_SIDE**2)/4)
-    global HEXAGON_SMALL_HEIGHT
-    HEXAGON_SMALL_HEIGHT = math.sqrt(HEXAGON_SIDE**2 - HEXAGON_APOTHEM**2)
-    global CROP_ROOM
-    CROP_ROOM = math.floor(IMAGE_SIZE - 2*HEXAGON_APOTHEM)
-
-def read_alphabet(vowels, consonants, modifiers, breaks):
-    """ Reads the file in ALPHABET_FILE and returns an array of with
+def read_alphabet(vowels, consonants, modifiers, breaks, config):
+    """ Reads the file alphabet file and returns an array of with
     the symbols for each individual letter."""
 
-    file = open(ALPHABET_FILE, "r", encoding="utf-8")
+    fileName = config["ProgramSettings"]["AlphabetFile"]
+    file = open(fileName, "r", encoding="utf-8")
     n = 0
 
     for word in file:
@@ -58,24 +43,26 @@ def read_alphabet(vowels, consonants, modifiers, breaks):
         # Begin drawing each letter and assigning it to its corresponding
         # array
         if(type == "v"):
-            vowels[letter] = draw_symbol(parts)
+            vowels[letter] = draw_symbol(parts, config)
         elif(type == "c"):
-            consonants[letter] = draw_symbol(parts)
+            consonants[letter] = draw_symbol(parts, config)
         elif(type == "m"):
-            modifiers[letter] = draw_symbol(parts)
+            modifiers[letter] = draw_symbol(parts, config)
         elif(type == "b"):
-            breaks[letter] = draw_symbol(parts)
+            breaks[letter] = draw_symbol(parts, config)
         n += 1
     print("Reading Dictionary. Letters Read: {}".format(n))
     file.close()
     return [vowels, consonants, modifiers, breaks]
 
-def read_text(vowels, consonants, modifiers, breaks):
-    """ Main text parser function. Reads the text in TEXT_NAME and returns
+def read_text(vowels, consonants, modifiers, breaks, config):
+    """ Main text parser function. Reads the text in textName and returns
     the list of symbols that need to be printed"""
+    imageSize = config["ProgramSettings"].getint("ImageSize")
 
     print("Reading Text...")
-    textFile = open(TEXT_NAME, "r", encoding="utf-8")
+    textName = config["UserSettings"]["TextFile"]
+    textFile = open(textName, "r", encoding="utf-8")
     symbols = []
     n = 0
     k = 0
@@ -96,7 +83,7 @@ def read_text(vowels, consonants, modifiers, breaks):
             parts = []
             # Check if symbol is unknown and draws UnknownSymbol
             if line[0] == "$":
-                symbol = Image.new('RGBA',(IMAGE_SIZE, IMAGE_SIZE),(255, 255, 255, 0))
+                symbol = Image.new('RGBA',(imageSize, imageSize),(255, 255, 255, 0))
                 symbol.paste(modifiers["UnknownSymbol"],(0,0),modifiers["UnknownSymbol"])
                 symbols.append(symbol)
 
@@ -152,7 +139,7 @@ def read_text(vowels, consonants, modifiers, breaks):
                 parts.append(modifiers["ModifierNoVowel"])
 
             # The symbols are all put  together into the final syllable.
-            symbol = Image.new('RGBA',(IMAGE_SIZE, IMAGE_SIZE),(255, 255, 255, 0))
+            symbol = Image.new('RGBA',(imageSize, imageSize),(255, 255, 255, 0))
             for part in parts:
                 symbol.paste(part,(0,0),part)
             symbols.append(symbol)
@@ -176,33 +163,42 @@ def check_letter(letter, vowels, consonants, modifiers, breaks):
     else:
         return False
 
-def draw_symbol(numbers):
+def draw_symbol(numbers, config):
     # Takes numbers in an array and joins them into an image by using the corresponding
     # Parts/(number).png files.
-    symbol = Image.new('RGBA', (IMAGE_SIZE,IMAGE_SIZE),(255, 255, 255, 0))
+    imageSize = config["ProgramSettings"].getint("ImageSize")
+    cropRoom = config["HexagonMath"].getint("CropRoom")
+    symbol = Image.new('RGBA', (imageSize,imageSize),(255, 255, 255, 0))
 
     for number in numbers:
         imageName = 'Parts\{}.png'.format(number)
         part = Image.open(imageName)
         symbol.paste(part,(0,0),part)
+        part.close()
     
     # Trim the excess from the top and sides of the hexagon for easier pasting later on
-    symbol = symbol.crop((CROP_ROOM/2,0,IMAGE_SIZE - CROP_ROOM/2,IMAGE_SIZE))
+    symbol = symbol.crop((cropRoom/2,0,imageSize - cropRoom/2,imageSize))
     return symbol
 
 
-def draw_text(symbols):
+def draw_text(symbols, config):
     # Takes the list of symbols and transforms it into the contiguous text.
-    collumns = MAX_CHARACTERS_PER_LINE
-    if(len(symbols) < MAX_CHARACTERS_PER_LINE):
+    maxCharactersPerLine = config["UserSettings"].getint("CharactersPerLine")
+    hexagonApothem = config["HexagonMath"].getint("HexagonApothem")
+    lineWidth = config["ProgramSettings"].getint("LineWidth")
+    hexagonSide =  config["HexagonMath"].getint("HexagonSide")
+
+
+    collumns = maxCharactersPerLine
+    if(len(symbols) < maxCharactersPerLine):
         collumns = len(symbols)
 
     rows = math.ceil(len(symbols) / collumns)
     n = 0
 
-    image = create_canvas(rows, collumns)
-    if(GUIDES):
-        image = draw_guide(rows, collumns, len(symbols), image)
+    image = create_canvas(rows, collumns, config)
+    if(config["UserSettings"].getboolean("guides")):
+        image = draw_guide(rows, collumns, len(symbols), image, config)
 
     for r in range(0,rows):
         for c in range(0,collumns):
@@ -211,35 +207,47 @@ def draw_text(symbols):
                 print("Working on image {}/{}".format(n,len(symbols)))
                 return image
             # Decide the placement of each symbol with a bit of math. Hexagons are fun.
-            x = math.ceil(2*c*HEXAGON_APOTHEM - c*LINE_WIDTH
-                          - (r%2)*LINE_WIDTH/2 + HEXAGON_APOTHEM*(r%2))
-            y = math.ceil(r*(HEXAGON_SIDE - LINE_WIDTH/2)*3/2)
+            x = math.ceil(2*c*hexagonApothem - c*lineWidth
+                          - (r%2)*lineWidth/2 + hexagonApothem*(r%2))
+            y = math.ceil(r*(hexagonSide - lineWidth/2)*3/2)
             image.paste(symbols[n], (x, y), symbols[n])
             n += 1
 
     print("Working on image {}/{}".format(n,len(symbols)))
     return image
 
-def create_canvas(rows, columns):
-    # Creates the full size of the final image with a bit of hexagon math. Since the hexagon
-    # edges must overlap each other and each even numbered line has to be displaced, it
-    # requires some math.
+def create_canvas(rows, columns, config):
+    """ Creates the full size of the final image with a bit of hexagon math. Since the hexagon
+    edges must overlap each other and each even numbered line has to be displaced, it
+    requires some math."""
     # TODO: fix the canvas size in case of a small number of symbols.
-    w = math.ceil(2*HEXAGON_APOTHEM*columns - LINE_WIDTH*(columns-1) - LINE_WIDTH/2)
+
+    hexagonSmallHeight = config["HexagonMath"].getint("HexagonSmallHeight")
+    hexagonApothem = config["HexagonMath"].getint("HexagonApothem")
+    lineWidth = config["ProgramSettings"].getint("LineWidth")
+    hexagonSide =  config["HexagonMath"].getint("HexagonSide")
+
+    w = math.ceil(2*hexagonApothem*columns - lineWidth*(columns-1) - lineWidth/2)
     if(rows > 1):
-        w += math.ceil(HEXAGON_APOTHEM)
-    h = math.ceil(rows*(HEXAGON_SIDE - LINE_WIDTH/2)*3/2
-                  + HEXAGON_SMALL_HEIGHT + LINE_WIDTH)
+        w += math.ceil(hexagonApothem)
+    h = math.ceil(rows*(hexagonSide - lineWidth/2)*3/2
+                  + hexagonSmallHeight+ lineWidth)
 
     canvas = Image.new('RGBA',(w, h),(255, 255, 255, 255))
 
     return canvas
 
-def draw_guide(rows, collumns, length, canvas):
-    # If GUIDES = True, then this function will draw guidelines for the placement of each
+def draw_guide(rows, collumns, length, canvas, config):
+    # If guides == True, then this function will draw guidelines for the placement of each
     # hexagonal symbol.
+    imageSize = config["ProgramSettings"].getint("ImageSize")
+    hexagonApothem = int(config["HexagonMath"].getint("HexagonApothem"))
+    lineWidth = config["ProgramSettings"].getint("LineWidth")
+    hexagonSide =  config["HexagonMath"].getint("HexagonSide")
+    cropRoom = config["HexagonMath"].getint("CropRoom")
+
     guide = Image.open('Parts\guide.png')
-    guide = guide.crop((CROP_ROOM/2,0,IMAGE_SIZE - CROP_ROOM/2,IMAGE_SIZE))
+    guide = guide.crop((cropRoom/2,0,imageSize - cropRoom/2,imageSize))
     n = 0
 
     for r in range(0,rows):
@@ -249,9 +257,9 @@ def draw_guide(rows, collumns, length, canvas):
                 print("Working on guides {}/{}".format(n,length))
                 return canvas
             # TODO: Implement the following code as a function to avoid rewriting it.
-            x = math.ceil(2*c*HEXAGON_APOTHEM - c*LINE_WIDTH - (r%2)*LINE_WIDTH/2
-                          + HEXAGON_APOTHEM*(r%2))
-            y = math.ceil(r*(HEXAGON_SIDE - LINE_WIDTH/2)*3/2)
+            x = math.ceil(2*c*hexagonApothem - c*lineWidth - (r%2)*lineWidth/2
+                          + hexagonApothem*(r%2))
+            y = math.ceil(r*(hexagonSide - lineWidth/2)*3/2)
             canvas.paste(guide, (x, y), guide)
             n += 1
 
@@ -260,22 +268,25 @@ def draw_guide(rows, collumns, length, canvas):
 
 
 def main():
-    read_config()
+    config = read_config()
 
     vowels = {}
     consonants = {}
     modifiers = {}
     breaks = {}
     [vowels, consonants, modifiers, breaks] = read_alphabet(vowels, consonants,
-                                                            modifiers, breaks)
+                                                            modifiers, breaks,
+                                                            config)
 
-    symbols = read_text(vowels, consonants, modifiers, breaks)
-    result = draw_text(symbols)
+    symbols = read_text(vowels, consonants, modifiers, breaks,config)
+    result = draw_text(symbols,config)
 
-    if(RESIZE): 
+    # Resize image if configuration is True
+    if(config["UserSettings"].getboolean("resize")): 
         print("Resizing...")
+        resizePercent = config["UserSettings"].getfloat("ResizePercent")
         h, w = result.size
-        new_size = (math.ceil(h*RESIZE_PERCENT), math.ceil(w*RESIZE_PERCENT))
+        new_size = (math.ceil(h*resizePercent), math.ceil(w*resizePercent))
         resizedResult = result.resize(new_size)
         #resizedResult = resizedResult.filter(ImageFilter.BoxBlur(2))
         print("Saving...")
